f0b5d6172cec4c1c00af507c52098f25e15cea12
andres0191 <andres_felipe0191@hotmail.com>
2023-05-30 18:01:31 -0500

    add find method to the project

--- js/script.js
+++ js/script.js
@@ -1,32 +1,48 @@
 
-function descuentos(){
-    alert('Este es un ejercicio en el cual se practicaron temas como condicionales, ciclos, e iteraciones. A continuacion vas a ingresar 3 precios diferentes, seguidos del valor de descuento que deseas aplicar.')
-const priceOne = window.prompt('ingresa un precio'),
-    priceTwo = window.prompt('ingresa el segundo precio'),
-    priceTrhee = window.prompt('ingresa el tercer precio'),
-    dto = window.prompt('Ingresa el valor de dto que desas aplicar'),
-    allPrice = [priceOne, priceTwo, priceTrhee]
+let productos = []
+//constructor
+function dona(sabor, topping, cobertura){
+        this.sabor = sabor;
+        this.topping = topping;
+        this.cobertura = cobertura;
+    }
+    
+function agregarDona(){
+    let agregarSabor  = prompt("Ingresa el sabor de tu dona. Puedes elegir entre:\nChocolate\nMiel Canela\nArequipe\nMoca\nFrutal\nMani")
+    let agregarCobertura = prompt("Ingresa la cobertura de tu dona. Puedes elegir entre:\nChocolate negro\nAzucar-Canela\nRelleno de Arequipe\nChocolate\nChocolate de colores\nChocolate Negro")
+    let agregarTopping = prompt("Ingresa el toping de tu dona. Puedes elegir entre:\nGrageas\nDecorado Chocolate\nRallas de Nutella\nMasmelos\nFruta Cristalizada\nMani")
 
-if(isNaN(allPrice[0]) || isNaN(allPrice[1]) || isNaN(allPrice[2]) || isNaN(dto) || allPrice=="" ){
-    alert(`debes ingresar solo numeros`)
-}else if(priceOne == 0 || priceTwo == 0 || priceTrhee == 0 ){
-    return alert('ingresa un dato valido')
+    let donaFinal = new dona(agregarSabor,agregarCobertura, agregarTopping)
+    console.log(donaFinal)
+    productos.push(donaFinal) 
+}
+
+//agregar nuevo producto
+let otraDona = function() {
+    confirm('Deseas agregar otra dona?')
+    if(confirm() ==  true){
+        do{
+            agregarDona()
+        }while(confirm() == true)
     }else{
-        for(let i =0; i<allPrice.length; i++){
-            let descuento= allPrice[i]*`0.${dto}`
-                descuento = parseInt(descuento.toFixed(2))
-                if(descuento==NaN){
-                    alert('ingresa un dato valido')
-                    break
-                }else{
-                alert(`El valor de dto aplicado al precio de $ ${allPrice[i]} sera de $ ${descuento}, por lo tanto debes cancelar: $ ${allPrice[i] - descuento}`)
-                }
-        }
+        alert('gracias por tu compra')
     }
 }
 
-alert(descuentos())
+//buscar producto
+function buscarDona(){
+    let param = prompt("Si deseas saber como elegiste tu dona, ingresa el sabor base que elegiste y podras ver tu eleccion completa. Recuerda que los sabores son: \nChocolate\nMiel Canela\nArequipe\nMoca\nFrutal\nMani")
+    const resultado = productos.find((dona) => dona.sabor === param)
+    alert(`Elegiste tu dona con los siguientes ingredientes:  ${resultado.sabor} - ${resultado.topping} - ${resultado.cobertura}`)
+    alert('gracias por tu compra')
+}
 
+function ejecutar(){
+    alert('Bienvenido a Donitas, a continuacion presiona enter para ingresar los sabores, cobertura y topings de tu ordern.')
+    agregarDona()
+    otraDona()
+    buscarDona()
 
+}
 
-
+ejecutar()
