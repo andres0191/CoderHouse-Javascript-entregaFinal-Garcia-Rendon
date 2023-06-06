e92b246d80f57dba1a5bc8b4df7db8b042f4740b
andres0191 <andres_felipe0191@hotmail.com>
2023-06-06 17:11:37 -0500

    modify code of js file

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 
-let productos = []
-//constructor
-function dona(sabor, topping, cobertura){
-        this.sabor = sabor;
-        this.topping = topping;
-        this.cobertura = cobertura;
+const productos = []
+
+function Dona(sabor, topping, cobertura){
+        this.sabor = sabor
+        this.topping = topping
+        this.cobertura = cobertura
     }
     
-function agregarDona(){
+function AgregarDona(){
     let agregarSabor  = prompt("Ingresa el sabor de tu dona. Puedes elegir entre:\nChocolate\nMiel Canela\nArequipe\nMoca\nFrutal\nMani")
     let agregarCobertura = prompt("Ingresa la cobertura de tu dona. Puedes elegir entre:\nChocolate negro\nAzucar-Canela\nRelleno de Arequipe\nChocolate\nChocolate de colores\nChocolate Negro")
     let agregarTopping = prompt("Ingresa el toping de tu dona. Puedes elegir entre:\nGrageas\nDecorado Chocolate\nRallas de Nutella\nMasmelos\nFruta Cristalizada\nMani")
 
-    let donaFinal = new dona(agregarSabor,agregarCobertura, agregarTopping)
+    let donaFinal = new Dona(agregarSabor,agregarCobertura, agregarTopping)
     console.log(donaFinal)
     productos.push(donaFinal) 
 }
 
-//agregar nuevo producto
-let otraDona = function() {
-    confirm('Deseas agregar otra dona?')
-    if(confirm() ==  true){
-        do{
-            agregarDona()
-        }while(confirm() == true)
+function OtraDona() {
+   let pregunta = confirm('Deseas agregar otra dona?')
+    if(pregunta){
+        AgregarDona()
     }else{
         alert('gracias por tu compra')
     }
 }
 
-//buscar producto
-function buscarDona(){
+function BuscarDona(){
     let param = prompt("Si deseas saber como elegiste tu dona, ingresa el sabor base que elegiste y podras ver tu eleccion completa. Recuerda que los sabores son: \nChocolate\nMiel Canela\nArequipe\nMoca\nFrutal\nMani")
     const resultado = productos.find((dona) => dona.sabor === param)
-    alert(`Elegiste tu dona con los siguientes ingredientes:  ${resultado.sabor} - ${resultado.topping} - ${resultado.cobertura}`)
+    if(resultado){
+        alert(`Elegiste tu dona con los siguientes ingredientes:  ${resultado.sabor} - ${resultado.topping} - ${resultado.cobertura}`)
+    }else{
+        alert(`No hemos podido encontrat tu eleccion`)
+    }    
     alert('gracias por tu compra')
 }
 
-function ejecutar(){
+function Ejecutar(){
     alert('Bienvenido a Donitas, a continuacion presiona enter para ingresar los sabores, cobertura y topings de tu ordern.')
-    agregarDona()
-    otraDona()
-    buscarDona()
+    AgregarDona()
+    OtraDona()
+    BuscarDona()
 
 }
 
-ejecutar()
+Ejecutar()
