9486e9ef3a4e64af230f01f5fc2cedc01a9927dd
andres0191 <andres_felipe0191@hotmail.com>
2023-06-19 18:04:44 -0500

    start of the third pre-delivery development

--- js/script.js
+++ js/script.js
@@ -1,48 +1,25 @@
+const titulo = document.getElementById('titulo')
+titulo.textContent = 'Donitas'
 
-const productos = []
+console.log(arrayPorductos)
 
-function Dona(sabor, topping, cobertura){
-        this.sabor = sabor
-        this.topping = topping
-        this.cobertura = cobertura
-    }
-    
-function AgregarDona(){
-    let agregarSabor  = prompt("Ingresa el sabor de tu dona. Puedes elegir entre:\nChocolate\nMiel Canela\nArequipe\nMoca\nFrutal\nMani")
-    let agregarCobertura = prompt("Ingresa la cobertura de tu dona. Puedes elegir entre:\nChocolate negro\nAzucar-Canela\nRelleno de Arequipe\nChocolate\nChocolate de colores\nChocolate Negro")
-    let agregarTopping = prompt("Ingresa el toping de tu dona. Puedes elegir entre:\nGrageas\nDecorado Chocolate\nRallas de Nutella\nMasmelos\nFruta Cristalizada\nMani")
-
-    let donaFinal = new Dona(agregarSabor,agregarCobertura, agregarTopping)
-    console.log(donaFinal)
-    productos.push(donaFinal) 
+const container = document.getElementById('container')
+function retornoCardHTML(producto){
+    return `<div class="card" style="width: 18rem;margin-top: 5%;margin-bottom: 5%">
+    <img src="${producto.imagen}" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">${producto.base}</h5>
+      <p class="card-text">${producto.description}</p>
+      <a href="#" class="btn btn-primary" id="${producto.codigo}>Agregar</a>
+    </div>
+  </div>`
 }
 
-function OtraDona() {
-   let pregunta = confirm('Deseas agregar otra dona?')
-    if(pregunta){
-        AgregarDona()
-    }else{
-        alert('gracias por tu compra')
-    }
+function cargarPorductos(){
+    container.innerHTML = ''
+    arrayPorductos.forEach((producto) => {
+        container.innerHTML += retornoCardHTML(producto)
+    });
 }
+cargarPorductos()
 
-function BuscarDona(){
-    let param = prompt("Si deseas saber como elegiste tu dona, ingresa el sabor base que elegiste y podras ver tu eleccion completa. Recuerda que los sabores son: \nChocolate\nMiel Canela\nArequipe\nMoca\nFrutal\nMani")
-    const resultado = productos.find((dona) => dona.sabor === param)
-    if(resultado){
-        alert(`Elegiste tu dona con los siguientes ingredientes:  ${resultado.sabor} - ${resultado.topping} - ${resultado.cobertura}`)
-    }else{
-        alert(`No hemos podido encontrat tu eleccion`)
-    }    
-    alert('gracias por tu compra')
-}
-
-function Ejecutar(){
-    alert('Bienvenido a Donitas, a continuacion presiona enter para ingresar los sabores, cobertura y topings de tu ordern.')
-    AgregarDona()
-    OtraDona()
-    BuscarDona()
-
-}
-
-Ejecutar()
