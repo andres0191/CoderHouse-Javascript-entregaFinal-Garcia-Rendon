9a55312b9705e10c3073165b0476c138863fe6af
andres0191 <andres_felipe0191@hotmail.com>
2023-06-20 10:40:43 -0500

    update checkout file

--- js/checkout.js
+++ js/checkout.js
@@ -2,10 +2,11 @@ const tbody = document.querySelector('tbody')
 
  function mostrarProductos(productCar){
     return`<tr>
-                <td>${productCar.imagen}</td>
+                <td><img class="donitaIcon" src="${productCar.imagen}"></td>
                 <td>${productCar.base}</td>
                 <td>${productCar.description}</td>
                 <td>${productCar.precio}</td>
+                <td><a><img style="width:35px" src="./src/img/eliminar.png"></a></td>
            </tr>`
 }
 
