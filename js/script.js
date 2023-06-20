0b28bef265b158f140456723d25b32197bfaefb1
andres0191 <andres_felipe0191@hotmail.com>
2023-06-19 19:55:34 -0500

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 const titulo = document.getElementById('titulo')
 titulo.textContent = 'Donitas'
 
-console.log(arrayPorductos)
+const bienvenidos = document.getElementById('bienvenido')
+const texto = document.createTextNode('A continuacion se presenta la tercera pre-entrega para el proyecto final')
+bienvenidos.appendChild(texto) 
+
+const description = document.getElementById('nosotros')
+description.textContent = `Donitas con sabor a chocolate, miel canela, arequipe, mocca, frutal y maní es una iniciativa novedosa que busca satisfacer el paladar de los amantes de las donitas y ofrecer una experiencia gastronómica única en el mercado de alimentos. La combinación de sabores dulces y salados ofrece una variedad de opciones para todo tipo de gustos. Nuestro principal <b>objetivo</b> es proporcionar una variedad de opciones de sabores para sus clientes. Para ello, se utilizarán ingredientes de alta calidad y se seguirán procesos de producción estrictos para garantizar la frescura y calidad de los productos.<br><br>Somos un negocio digital, por ello, utilizamos plataformas como Facebook, Twitter, Instagram, WhatsApp, Telegram, TickTock para promocionar nuestros productos y generar interaccion con nuestros clientes. `
+
+
 
 const container = document.getElementById('container')
-function retornoCardHTML(producto){
+function retornoCardHTML(product){
     return `<div class="card" style="width: 18rem;margin-top: 5%;margin-bottom: 5%">
-    <img src="${producto.imagen}" class="card-img-top">
+    <img src="${product.imagen}" class="card-img-top">
     <div class="card-body">
-      <h5 class="card-title">${producto.base}</h5>
-      <p class="card-text">${producto.description}</p>
-      <a href="#" class="btn btn-primary" id="${producto.codigo}>Agregar</a>
+      <h5 class="card-title">${product.base}</h5>
+      <p class="card-text">${product.description}</p>
+      <a class="btn btn-primary" id="${product.ref}">Agregar</a>
     </div>
   </div>`
 }
 
 function cargarPorductos(){
     container.innerHTML = ''
-    arrayPorductos.forEach((producto) => {
+    arraySabores.forEach((producto) => {
         container.innerHTML += retornoCardHTML(producto)
     });
+    activarClick()
 }
 cargarPorductos()
 
+function activarClick(){
+  const btns = document.querySelectorAll('a.btn.btn-primary')
+  console.log(btns)
+}
