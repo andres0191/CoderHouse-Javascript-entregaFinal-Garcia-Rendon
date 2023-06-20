de536bb3ae1eb757b9965f97d0b883f5edaf3653
andres0191 <andres_felipe0191@hotmail.com>
2023-06-20 00:18:53 -0500

    update styles and modify html, add count of prodcuts and modify navegation to ckecount.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ const titulo = document.getElementById('titulo')
 titulo.textContent = 'Donitas'
 
 const imgLogo = document.querySelector('img#logo')
+const logoCesta = document.querySelector('img#logoCesta')
+const vlorCount = document.querySelector('spam#count')
 
+const nosotros = document.getElementById('nosotros')
+nosotros.textContent = `¡Bienvenidos a Donitas!En Sweet Delights, somos una microempresa dedicada a la producción y venta de deliciosas mini donas. Nuestro amor por los postres nos ha llevado a crear un concepto único que combina la pasión por la repostería con la creatividad en cada una de nuestras creaciones. En cada paso del proceso de elaboración, nos esforzamos por brindar la máxima calidad. Utilizamos ingredientes frescos y de la más alta calidad para asegurar un sabor inigualable en cada bocado. Nuestras mini donas se hornean diariamente en nuestras instalaciones, lo que garantiza su frescura y textura irresistibles. La variedad es uno de nuestros pilares fundamentales. Ofrecemos una amplia gama de sabores exquisitos y coberturas tentadoras que te dejarán sin palabras. Desde clásicas como glaseado de chocolate y azúcar en polvo hasta opciones más audaces como caramelo salado y crema de avellanas, tenemos algo para satisfacer todos los gustos.`
 
 const container = document.getElementById('container')
 function retornoCardHTML(product){
@@ -48,12 +52,22 @@ function activarClick(){
       console.log(JSON.stringify(carShop))
     })
   }
+  let contador = 0
+  btns.forEach(boton => {
+    boton.addEventListener('click', function(e) {
+      const estilos = e.currentTarget.classList
+      if(estilos.contains('addProdcut')){
+        contador++
+      }
+      vlorCount.textContent = contador
+    })
+  })
 }
 
-imgLogo.addEventListener('click', ()=> {
+logoCesta.addEventListener('click', ()=> {
   location.href = 'checkout.html'
 })
 
-imgLogo.addEventListener('mousemove', (e)=> {
-  imgLogo.title = 'Ver Donitas'
+logoCesta.addEventListener('mousemove', (e)=> {
+  logoCesta.title = 'Ver Donitas'
 })
